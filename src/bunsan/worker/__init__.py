385b06fcd9e4426c42af409c21347d6d4f18769b
Filippov Aleksey <sarum9in@gmail.com>
2012-11-07 03:04:57 +0400

    __main__ code was moved into cli module.

--- src/bunsan/worker/__init__.py
+++ src/bunsan/worker/__init__.py
@@ -9,8 +9,6 @@ import threading
 import tempfile
 import subprocess
 
-import bunsan.pm
-
 from bunsan.worker.dcs import *
 from bunsan.worker.callback import *
 
@@ -187,31 +185,3 @@ class Worker(object):
                     for worker in workers:
                         worker.join()
                     _log("Completed.")
-
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser("bunsan::worker")
-    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.0.1', help="version information")
-    parser.add_argument('-l', '--listen', action='store', dest='addr', help='Listen on addr:port', required=True)
-    parser.add_argument('-d', '--hub', action='store', dest='hub_uri', help='hub xmlrpc interface', required=True)
-    parser.add_argument('-m', '--machine', action='store', dest='machine', help='machine name', required=True)
-    parser.add_argument('-c', '--worker-count', action='store', dest='worker_count', type=int, help='worker count', default=1)
-    parser.add_argument('-r', '--repository-config', action='store', dest='repository_config', help='path to repository config', required=True)
-    parser.add_argument('-s', '--resource', action='append', dest='resources', help='resources in format resource_id=resource_uri')
-    parser.add_argument('-t', '--tmpdir', action='store', dest='tmpdir', help='temporary directory path', default='/tmp')
-    args = parser.parse_args()
-    host, port = tuple(args.addr.split(':'))
-    port = int(port)
-    def split_resource(s):
-        pos = s.index('=')
-        return (s[:pos], s[pos + 1:])
-    resources = list(map(split_resource, args.resources or []))
-    worker = Worker(
-        addr=(host, port),
-        worker_count=args.worker_count,
-        resources=resources,
-        repository=bunsan.pm.Repository(args.repository_config),
-        hub=Hub(hub_uri=args.hub_uri, machine=args.machine),
-        tmpdir=args.tmpdir)
-    worker.serve_forever(signals=[signal.SIGTERM])
